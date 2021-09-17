4258496afc13bb7263943b059904652ef6c26c56
Bhagvat <bhagvatmutthe6@gmail.com>
2021-09-17 10:29:03 +0530

    Initial commit

---
+++ app/src/main/java/com/bm/covid_vaccination_by_pin/dev_info.java
@@ -0,0 +1,14 @@
+package com.bm.covid_vaccination_by_pin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dev_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_info);
+    }
+}
